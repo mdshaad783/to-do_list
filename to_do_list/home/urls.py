26113431465120bb691cb64dd97c85e996a7d448
Md Shaad Rehan <113508620+mdshaad783@users.noreply.github.com>
2023-09-19 15:39:02 +0530

    Delete to_do_list directory

--- to_do_list/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('tasklist', views.tasklist, name='tasklist'),
-    path('delete_task/<id>/', views.delete_task, name='delete_task')
-]
